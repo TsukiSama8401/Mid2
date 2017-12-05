4f9bacba23494da2cfbe1e965ea94628cd0627ed
yohanei <sleepcat102352689@gmail.com>
2017-12-05 11:36:55 +0800

    Mid2

--- app/src/main/java/com/fju/mid2/Func3Activity.java
+++ app/src/main/java/com/fju/mid2/Func3Activity.java
@@ -1,8 +1,10 @@
 package com.fju.mid2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Func3Activity extends AppCompatActivity {
 
@@ -10,9 +12,18 @@ public class Func3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func3);
+        Button button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent();
+                intent.setClass(Func3Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void func3(View view){
-
+        finish();
     }
 }
--- app/src/main/java/com/fju/mid2/Func8Activity.java
+++ app/src/main/java/com/fju/mid2/Func8Activity.java
@@ -1,7 +1,10 @@
 package com.fju.mid2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Func8Activity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Func8Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func8);
+        Button button11 = (Button) findViewById(R.id.button11);
+        button11.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent();
+                intent.setClass(Func8Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void func8(View view){
+        finish();
     }
 }
